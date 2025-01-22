a8ae32544a97cd0f141d1999141ef3e3d993b380
Dheeraj Sharma <dheeraj9410096874@gmail.com>
2025-01-22 11:44:27 +0530

    16 Level 1 practice problems done.

---
+++ MaximumHandshakes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MaximumHandshakes {
+    static void Main() {
+        Console.WriteLine("Enter the number of students:");
+        int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+        
+        int maxHandshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+        
+        Console.WriteLine($"The maximum number of possible handshakes is {maxHandshakes}.");
+    }
+}
